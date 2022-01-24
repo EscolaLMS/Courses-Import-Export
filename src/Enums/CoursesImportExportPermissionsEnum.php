e50d7e08684493dd10733ccfa271abccf32c5c2f
KrzysztofDziedziechEscolasoft <96292232+KrzysztofDziedziechEscolasoft@users.noreply.github.com>
2022-01-24 12:14:28 +0100

    #94 - permissions renamed (#15)
    * #94 - permissions renamed
    
    * #94 - fix test
    
    Co-authored-by: KD <do>

--- src/Enums/CoursesImportExportPermissionsEnum.php
+++ src/Enums/CoursesImportExportPermissionsEnum.php
@@ -6,7 +6,7 @@ use EscolaLms\Core\Enums\BasicEnum;
 
 class CoursesImportExportPermissionsEnum extends BasicEnum
 {
-    public const COURSES_IMPORT = 'courses-import-export_import';
-    public const COURSES_EXPORT = 'courses-import-export_export';
-    public const COURSES_EXPORT_OWNED = 'courses-import-export_export_authored';
+    public const COURSES_IMPORT = 'course-import-export_import';
+    public const COURSES_EXPORT = 'course-import-export_export';
+    public const COURSES_EXPORT_OWNED = 'course-import-export_export_authored';
 }
