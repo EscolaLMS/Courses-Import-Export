7f54e0dc3cb4d7e455abc4a4fda643f77ffb3ba0
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-05-05 14:27:10 +0200

    Add routes from Core

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,10 @@
 <?php
 
 use EscolaLms\CoursesImportExport\Http\Controllers\CourseExportImportAPIController;
-use Illuminate\Support\Facades\Route;
+use EscolaLms\Core\Http\Facades\Route;
 
 // admin endpoints
-Route::group(['middleware' => ['auth:api'], 'prefix' => 'api/admin'], function () {
+Route::group(['middleware' => Route::apply(['auth:api']), 'prefix' => 'api/admin'], function () {
     Route::get('courses/{course}/clone', [CourseExportImportAPIController::class, 'clone']);
     Route::get('courses/{course}/export', [CourseExportImportAPIController::class, 'export']);
     Route::post('courses/zip/import', [CourseExportImportAPIController::class, 'import']);
