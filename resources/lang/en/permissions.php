9a7d9c9599172afd5bf48e5a78d1cc6c2002150c
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-11-18 15:42:19 +0100

    Add translations (#26)

---
+++ resources/lang/en/permissions.php
@@ -0,0 +1,10 @@
+<?php
+
+use EscolaLms\CoursesImportExport\Enums\CoursesImportExportPermissionsEnum;
+
+return [
+    CoursesImportExportPermissionsEnum::COURSES_IMPORT => 'Import course',
+    CoursesImportExportPermissionsEnum::COURSES_EXPORT => 'Export course',
+    CoursesImportExportPermissionsEnum::COURSES_EXPORT_OWNED => 'Export owned course',
+    CoursesImportExportPermissionsEnum::COURSES_CLONE => 'Clone course',
+];
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -54,6 +54,7 @@ class TestCase extends \EscolaLms\Core\Tests\TestCase
         $app['config']->set('database.connections.mysql.strict', false);
         $app['config']->set('app.debug', (bool) env('APP_DEBUG', true));
         $app['config']->set('escolalms.tags.ignore_migrations', false);
+        $app['config']->set('hh5p.h5p_export', true);
         $app['config']->set('filesystems.disks.local', [
             'driver' => 'local',
             'root' => storage_path('app/public'),
